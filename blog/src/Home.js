3784c1b1f5809755d646fbc8a6da6e687a7a716c
Zambetti Giorgio <giorgio.zambo2@gmail.com>
2024-03-05 13:33:13 +0100

    Initial commit

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -2,7 +2,8 @@ export function Home() {
     return(
         <p>
             Welcome to my blog!<br/>
-            <b>With an edit</b>
+            <b>With an edit</b><br/>
+            This is quite React-ive
         </p>
     );
 }
