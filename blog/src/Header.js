8db55803f14ce038b94ed1990e1ac55477bad33c
Zambetti Giorgio <giorgio.zambo2@gmail.com>
2024-06-27 11:12:44 +0200

    feat: improve header, add pdf

--- blog/src/Header.js
+++ blog/src/Header.js
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import {
+    Nav,
+    Navbar,
+    NavDropdown
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 export function Header(){
     const style = {
         padding: "6px",
         textAlign: "center",
-        background: "#F2BF3C",
+        background: "#72a9c4",
         color: "white",
         fontSize: "30px",
         display: "flex",
@@ -13,19 +20,42 @@ export function Header(){
     }
 
     const zambettiGiorgioStyle = {
-        fontFamily: "inter", fontSize: 36, color: "#EDEDED", fontWeight:"bold"
+        fontFamily: "inter", fontSize: "1.2em", color: "#EDEDED", fontWeight:"bold"
+    }
+    const dotComStyle = {
+        fontFamily: "inter", fontSize: "0.8em", color: "#1B1A20", fontWeight:"500", textDecoration:"none", verticalAlign:"middle"
     }
     const littleTextStyle = {
-        fontFamily: "inter", fontSize: 20, color: "#1B1A20", fontWeight:"500", textDecoration:"none", verticalAlign:"middle"
+        fontWeight:"500", textDecoration:"none", color:"inherit"
     }
-
-    return <div style={style}>
+/*<div style={style}>
         <div>
-            <Link to="/" style={{textDecoration: "none"}}> <span style={zambettiGiorgioStyle}>ZambettiGiorgio</span><span style={littleTextStyle}>.com</span> </Link>
 
-            <Link to="/" style={{...littleTextStyle,  fontWeight:"bold", marginLeft: "100px"}}> articles </Link>
-            <Link to="/AboutMe" style={{...littleTextStyle,  fontWeight:"bold", marginLeft: "20px"}}> about me </Link>
+
+
+            <Link to="/AboutMe" style={{...littleTextStyle,  fontWeight:"bold", marginLeft: "20px"}}> About Me </Link>
+            <a href={"/algorithmic_riddles.pdf"} target="_blank" rel="noreferrer" style={{...littleTextStyle,  fontWeight:"bold", marginLeft: "20px"}}> Algorithmic Riddles </a>
         </div>
-
     </div>
+
+        */
+    return(
+        <div className="Main">
+            <Navbar
+                className="Navy"
+                collapseOnSelect
+                expand="full"
+                style={style}
+            >
+                <Navbar.Brand href="#home"> <Link to="/" style={{textDecoration: "none"}}> <span style={zambettiGiorgioStyle}>ZambettiGiorgio</span><span style={dotComStyle}>.com</span> </Link></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link> <Link to="/AboutMe" style={littleTextStyle}> About Me </Link></Nav.Link>
+                        <Nav.Link> <Link to="/" style={littleTextStyle}> Articles </Link></Nav.Link>
+                        <Nav.Link href="/algorithmic_riddles.pdf" target="_blank"> <a style={littleTextStyle}>Algorithmic Riddles</a> </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+    </div>)
 }
--- blog/src/pages/AboutMe.js
+++ blog/src/pages/AboutMe.js
@@ -2,11 +2,18 @@
 export const AboutMe = () => {
     const text = "<space intentionally left blank for minimalistic purposes>"
     return <div style={{overflow:"hidden"}}>
-        <div style={{display:"flex"}}>
-            <img style={{borderRadius: "50%", width:200, marginLeft:"75%", position:"absolute", marginTop:70, boxShadow:"0px 4px 4px"}} alt="Giorgio" src="/imgs/propic.jpg"/>
-        </div>
         <h1 style={{fontFamily: "inter", fontWeight:500, textAlign:"center", marginTop:70}}>ABOUT ME</h1>
-        <p style={{margin:"auto", width:"50%"}}>I’m Zambetti Giorgio, a Computer Science student based in Italy. Here I share my projects and some interactive explainations of the algorithms I learn about. I hope that you will like them!  </p>
+        <div style={{display:"flex", flexWrap:"wrap"}}>
+            <div style={{flex:3, minWidth:"70%"}}>
+                <p style={{margin:"auto", maxWidth:"70%"}}>I’m Zambetti Giorgio, a Computer Science student based in Italy. Here I share my projects and some interactive explanations of the algorithms I learn about. I hope you will like them!  </p>
+            </div>
+            <div style={{flex:1}}>
+                <img style={{borderRadius: "50%", width:200, display:"block", margin:"auto",boxShadow:"0px 4px 4px"}} alt="Giorgio" src="/imgs/propic.jpg"/>
+            </div>
+
+
+
+        </div>
 
         <p style={{color:"#787878", textAlign:"center", padding:"top", marginTop:200}}>{text}</p>
     </div>
