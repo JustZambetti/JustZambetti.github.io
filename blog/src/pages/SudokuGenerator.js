f3f069d01b88423b932a95bb0601d9713df5ac3c
Zambetti Giorgio <giorgio.zambo2@gmail.com>
2024-07-07 20:07:08 +0200

    feat: improve sudoku page

--- blog/src/pages/SudokuGenerator.js
+++ blog/src/pages/SudokuGenerator.js
@@ -4,20 +4,22 @@ import {shuffleArray} from "../helpFunctions";
 import {Button, ButtonGroup} from "react-bootstrap";
 import {Sudoku} from "../sudoku";
 
-const sketch = (p)=> {
+const sketch = (p) => {
     let canvasWidth, canvasHeight
-    let gridSize = 9, cellSize, gridPixelSize
+    let gridSize = 9, cellSize, gridPixelSize, fontSize
     let margin = 10
-    let thickLineWeight = 3
+    let thickLineWeight = 2.5
     let thinLineWeight = 0.8
-    let sudoku = []
+    let sudoku, solution
 
 
-    let solution
     p.setup = () => {
-        canvasWidth = canvasHeight = Math.min(600, 0.6*Math.min(p.displayWidth, p.displayHeight))
+        canvasWidth = canvasHeight = Math.min(600, 0.6 * Math.min(p.displayWidth, p.displayHeight))
         cellSize = Math.min(0.95 * canvasWidth / gridSize, 0.95 * canvasHeight / gridSize)
-        gridPixelSize = gridSize*cellSize;
+        fontSize = 0.7* cellSize
+        thickLineWeight = 0.04*cellSize
+        thinLineWeight = 0.015*cellSize
+        gridPixelSize = gridSize * cellSize;
         p.easySetup()
         p.createCanvas(canvasWidth, canvasHeight)
     }
@@ -49,9 +51,9 @@ const sketch = (p)=> {
     }
 
     let selectedCell = null
-    p.mouseClicked = ()=> {
-        let x =  Math.floor((p.mouseX - margin) / cellSize)
-        let y =Math.floor((p.mouseY - margin) / cellSize)
+    p.mouseClicked = () => {
+        let x = Math.floor((p.mouseX - margin) / cellSize)
+        let y = Math.floor((p.mouseY - margin) / cellSize)
 
         if (x >= 0
             && x < gridSize
@@ -69,9 +71,16 @@ const sketch = (p)=> {
         p.mouseClicked()
     }
 
+    p.keyTyped = () => {
+        if(p.key >= '1' && p.key <= '9')
+            p.setValue(p.key-'0')
+    }
+
     function drawGrid() {
         p.background(255)
         p.translate(margin, margin)
+
+
 
         if (selectedCell) {
             p.push()
@@ -83,9 +92,19 @@ const sketch = (p)=> {
             p.rect(selectedCell.x * cellSize, 0, cellSize, gridPixelSize)
 
             p.fill("#abd3f5")
-            p.rect(selectedCell.x * cellSize, selectedCell.y * cellSize, cellSize, cellSize)
+            const selectedCellValue = sudoku.get(selectedCell.x, selectedCell.y)
+            if (selectedCellValue !== 0) {
+                for (let x = 0; x < gridSize; x++)
+                    for (let y = 0; y < gridSize; y++)
+                        if (sudoku.get(x, y) === selectedCellValue)
+                            p.rect(x * cellSize, y * cellSize, cellSize, cellSize)
+            } else {
+                p.rect(selectedCell.x * cellSize, selectedCell.y * cellSize, cellSize, cellSize)
+                p.rect(selectedCell.x * cellSize, selectedCell.y * cellSize, cellSize, cellSize)
+            }
             p.pop()
         }
+        p.avaiables = [1,2,3]
 
         for (let i = 0; i <= gridSize; i++) {
             const cord = i * cellSize;
@@ -115,10 +134,15 @@ const sketch = (p)=> {
         for (let x = 0; x < gridSize; x++)
             for (let y = 0; y < gridSize; y++) {
                 p.textAlign(p.CENTER, p.CENTER);
-                p.textSize(40);
-                if(sudoku.get(x,y))
-                    p.text(sudoku.get(x,y), x*cellSize, y*cellSize, cellSize, cellSize)
-        }
+                p.textSize(fontSize);
+                if (sudoku.get(x, y))
+                    p.text(sudoku.get(x, y), x * cellSize, y * cellSize, cellSize, cellSize)
+            }
+    }
+    let avaiable = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    let setAvaiable
+    p.setSetAvailable = (set) => {
+        setAvaiable = set
     }
     function drawHorizontalGridLine(y, isThick) {
         drawGridLine(0, y, gridPixelSize, y, isThick)
@@ -128,7 +152,7 @@ const sketch = (p)=> {
         drawGridLine(x, 0, x, gridPixelSize, isThick)
     }
 
-    function drawGridLine(x1, y1, x2, y2, isThick){
+    function drawGridLine(x1, y1, x2, y2, isThick) {
         p.push()
         p.strokeWeight(isThick ? thickLineWeight : thinLineWeight)
         p.stroke(isThick ? "#3d3d3d" : "#c7c7c7")
@@ -143,8 +167,8 @@ const sketch = (p)=> {
         let x = Math.floor(i % 9)
         let y = Math.floor(i / 9)
 
-        if(sudoku.get(x,y))
-            findRandomSudokuSolution(i+1)
+        if (sudoku.get(x, y))
+            findRandomSudokuSolution(i + 1)
 
         let shuffledArray = Array.from(enumerateBinaryBitsSudoku(sudoku.findImmediatelyPossibleValues(x, y)));
         shuffleArray(shuffledArray)
@@ -160,26 +184,25 @@ const sketch = (p)=> {
         return false
     }
 
-    function solutionsCount(i = 0){
-        if(sudoku.isFull())
+    function solutionsCount(i = 0) {
+        if (sudoku.isFull())
             return 1
 
-        let x = Math.floor(i%9)
-        let y = Math.floor(i/9)
+        let x = Math.floor(i % 9)
+        let y = Math.floor(i / 9)
 
-        if(sudoku.get(x,y))
-            return solutionsCount(i+1)
+        if (sudoku.get(x, y))
+            return solutionsCount(i + 1)
 
         let shuffledArray = Array.from(enumerateBinaryBitsSudoku(sudoku.findImmediatelyPossibleValues(x, y)));
         shuffleArray(shuffledArray)
 
         let solCount = 0
-        for(let value of shuffledArray){
+        for (let value of shuffledArray) {
             sudoku.move(x, y, value)
 
-            solCount += solutionsCount(i+1)
-            if(solCount > 1)
-            {
+            solCount += solutionsCount(i + 1)
+            if (solCount > 1) {
                 sudoku.move(x, y, 0)
 
                 return 2
@@ -190,51 +213,65 @@ const sketch = (p)=> {
         return solCount
     }
 
-    function removeSudokuCells(n){
-        if(n === 0){
+    function removeSudokuCells(n) {
+        if (n === 0) {
             return true
         }
 
-        if(solutionsCount() > 1)
+        if (solutionsCount() > 1)
             return false;
 
         let possibleRemovals = []
-        for(let x = 0; x < 9; x++)
-            for(let y = 0; y < 9; y++)
-                if(sudoku.get(x,y) !== 0)
-                    possibleRemovals.push(x+y*9)
+        for (let x = 0; x < 9; x++)
+            for (let y = 0; y < 9; y++)
+                if (sudoku.get(x, y) !== 0)
+                    possibleRemovals.push(x + y * 9)
 
-        if(possibleRemovals.length < n)
+        if (possibleRemovals.length < n)
             return false
         shuffleArray(possibleRemovals)
 
-        for(let index of possibleRemovals){
+        for (let index of possibleRemovals) {
             let x = index % 9
             let y = Math.floor(index / 9)
-            let temp = sudoku.get(x,y)
-            sudoku.move(x,y, 0)
+            let temp = sudoku.get(x, y)
+            sudoku.move(x, y, 0)
 
-            if(removeSudokuCells(n-1, sudoku) === true)
+            if (removeSudokuCells(n - 1, sudoku) === true)
                 return true
 
-            sudoku.move(x,y, temp)
+            sudoku.move(x, y, temp)
         }
 
         return false
     }
 
-    p.solve = ()=>{
+    p.solve = () => {
         findRandomSudokuSolution(0, sudoku)
     }
 
     let errors = []
     p.setValue = (v) => {
-        if(selectedCell && !sudoku.get(selectedCell.x,selectedCell.y)){
-            if(solution[selectedCell.x][selectedCell.y] === v){
-                sudoku.move(selectedCell.x,selectedCell.y, v)
+        if (selectedCell && !sudoku.get(selectedCell.x, selectedCell.y)) {
+            if (solution[selectedCell.x][selectedCell.y] === v) {
+
+                sudoku.move(selectedCell.x, selectedCell.y, v)
+
+                let counter = 0;
+                for (let x = 0; x < gridSize; x++)
+                    for (let y = 0; y < gridSize; y++)
+                        if(sudoku.get(x,y) === v)
+                            counter++
+
+                if(counter === 9){
+                    avaiable = avaiable.filter(value => value !== v)
+                    setAvaiable(avaiable)
+                    console.log("AAAAA")
+                }
+
+
                 errors = errors.filter(error => error.x !== selectedCell.x || error.y !== selectedCell.y)
-            }
-            else if(errors.some(error => error.x === selectedCell.x && error.y === selectedCell.y))
+            } else if (errors.some(error => error.x === selectedCell.x && error.y === selectedCell.y))
                 errors.find(error => error.x === selectedCell.x && error.y === selectedCell.y).value = v
             else
                 errors.push({x: selectedCell.x, y: selectedCell.y, value: v})
@@ -244,46 +281,48 @@ const sketch = (p)=> {
 
 function* enumerateBinaryBitsSudoku(number) {
     for (let i = 0; i < 9; i++)
-        if((number & (1 << i)) !== 0)
-            yield i+1
+        if ((number & (1 << i)) !== 0)
+            yield i + 1
 }
 
 export function SudokuGenerator() {
+    const [avaiable, setAvaiable] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    const [p5Instance, setP5Instance] = useState()
     const p5ContainerRef = useRef();
-    let p5Instance
     useEffect(() => {
-        p5Instance = new p5(sketch, p5ContainerRef.current)
+        let p5Instance = new p5(sketch, p5ContainerRef.current)
+        p5Instance.setSetAvailable(setAvaiable)
+        setP5Instance(p5Instance)
 
         return () => p5Instance.remove();
     }, [])
 
     return (
-        <div style={{marginTop:5}}>
+        <div style={{marginTop: 5}}>
             <div className="text-center">
                 <ButtonGroup>
-                    <Button size="lg" variant="outline-success"
-                            onClick={() => p5Instance.easySetup() }>
+                    <Button variant="outline-success"
+                            onClick={() => p5Instance.easySetup()}>
                         Easy
                     </Button>
-                    <Button  size="lg" variant="outline-warning"
-                            onClick={() => p5Instance.normalSetup() }>
+                    <Button variant="outline-warning"
+                            onClick={() => p5Instance.normalSetup()}>
                         Medium
                     </Button>
-                    <Button size="lg" variant="outline-danger"
-                            onClick={() => p5Instance.hardSetup() }>
+                    <Button variant="outline-danger"
+                            onClick={() => p5Instance.hardSetup()}>
                         Hard
                     </Button>
                 </ButtonGroup>
-
             </div>
 
-            <div className="App" style={{marginTop:5}} ref={p5ContainerRef}/>
+            <div className="App" style={{marginTop: 5}} ref={p5ContainerRef}/>
             <div className="text-center">
                 <ButtonGroup>
                     {
-                        [1,2,3,4,5,6,7,8,9].map((v) =>
-                            <Button size="lg" variant="outline-secondary"
-                                    onClick={() => p5Instance.setValue(v) }>
+                        avaiable.map((v) =>
+                            <Button variant="outline-secondary"
+                                    onClick={() => p5Instance.setValue(v)}>
                                 {v}
                             </Button>
                         )
@@ -293,6 +332,7 @@ export function SudokuGenerator() {
         </div>
     );
 }
+
 /*
 <Button variant="primary" size="lg"
                     onClick={() => p5Instance.solve() }>
--- blog/src/sudoku.js
+++ blog/src/sudoku.js
@@ -1,4 +1,4 @@
-export class Sudoku{
+export class Sudoku {
     constructor() {
         this.generateEmptyGrid()
         this.generateCounters()
@@ -8,39 +8,39 @@ export class Sudoku{
         this.emptyCells = 81
         this.grid = []
         for (let i = 0; i < 9; i++)
-            this.grid.push([0,0,0,0,0,0,0,0,0]);
+            this.grid.push([0, 0, 0, 0, 0, 0, 0, 0, 0]);
     }
 
     generateCounters() {
-        this.rows = [0,0,0,0,0,0,0,0,0]
-        this.columns = [0,0,0,0,0,0,0,0,0]
-        this.squares = [0,0,0,0,0,0,0,0,0]
+        this.rows = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        this.columns = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        this.squares = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     }
 
-     isImmediatelyPossible(x, y, value){
-        if(this.grid[x][y]) return false
-        if(this.isNthBit(this.rows[y], value)) return false
-        if(this.isNthBit(this.columns[x], value)) return false
-        const squareIndex = this.getSquareIndex(x,y)
-        if(this.isNthBit(this.squares[squareIndex], value)) return false
+    isImmediatelyPossible(x, y, value) {
+        if (this.grid[x][y]) return false
+        if (this.isNthBit(this.rows[y], value)) return false
+        if (this.isNthBit(this.columns[x], value)) return false
+        const squareIndex = this.getSquareIndex(x, y)
+        if (this.isNthBit(this.squares[squareIndex], value)) return false
     }
 
-    findImmediatelyPossibleValues(x, y){
-        const squareIndex = this.getSquareIndex(x,y)
+    findImmediatelyPossibleValues(x, y) {
+        const squareIndex = this.getSquareIndex(x, y)
         return ~(this.rows[y] | this.columns[x] | this.squares[squareIndex])
     }
 
-    getSquareIndex(x,y){
-        return Math.floor(x/3)+Math.floor(y/3)*3
+    getSquareIndex(x, y) {
+        return Math.floor(x / 3) + Math.floor(y / 3) * 3
     }
 
-    move(x,y,value){
-        const squareIndex = this.getSquareIndex(x,y)
+    move(x, y, value) {
+        const squareIndex = this.getSquareIndex(x, y)
 
-        if(this.grid[x][y] !== 0 && value === 0)
-            this.undoMove(x,y, this.grid[x][y])
+        if (this.grid[x][y] !== 0 && value === 0)
+            this.undoMove(x, y, this.grid[x][y])
 
-        else if(this.grid[x][y] === 0 && value !== 0){
+        else if (this.grid[x][y] === 0 && value !== 0) {
             this.emptyCells--
             this.rows[y] = this.setNthBit(this.rows[y], value)
             this.columns[x] = this.setNthBit(this.columns[x], value)
@@ -50,8 +50,8 @@ export class Sudoku{
         this.grid[x][y] = value
     }
 
-    undoMove(x,y, value){
-        const squareIndex = this.getSquareIndex(x,y)
+    undoMove(x, y, value) {
+        const squareIndex = this.getSquareIndex(x, y)
 
         this.emptyCells++
         this.rows[y] = this.resetNthBit(this.rows[y], value)
@@ -59,23 +59,25 @@ export class Sudoku{
         this.squares[squareIndex] = this.resetNthBit(this.squares[squareIndex], value)
     }
 
-    get(x,y){
+    get(x, y) {
         return this.grid[x][y]
     }
 
-    isNthBit(number, n){
+    isNthBit(number, n) {
         return (number >> (n - 1)) & 1;
     }
-    setNthBit(number, n){
+
+    setNthBit(number, n) {
         const mask = 1 << (n - 1);
         return number | mask;
     }
-    resetNthBit(number, n){
+
+    resetNthBit(number, n) {
         const mask = ~(1 << (n - 1));
         return number & mask;
     }
 
-    isFull(){
+    isFull() {
         return this.emptyCells === 0
     }
 }
